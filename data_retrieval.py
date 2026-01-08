7d7487f4cb28a38d82ae6bfe91a4cab059a7bb76
iangill1 <i.gill3@universityofgalway.ie>
2026-01-08 21:04:59 +0000

    fixed news retrieval method

--- data_retrieval.py
+++ data_retrieval.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 #retrieve historical stock price data from yfinance api
 def price_retrieval(ticker, start_date, end_date, interval):
-    data = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+    data = yf.download(ticker, start=start_date, end=end_date, interval=interval, auto_adjust=False)
     return data
 
 #retrieve news articles from finnhub api
@@ -16,6 +16,6 @@ def news_retrieval(ticker, start_date, end_date):
     #get my api key from env file
     finnhub_key = os.getenv("FINNHUB_API_KEY")
     #create finnhub client
-    finnhub_key = finnhub.Client(api_key=finnhub_key)
-    news = finnhub_key.company_news(ticker, _from=start_date, to=end_date)
+    finnhub_client = finnhub.Client(api_key=finnhub_key)
+    news = finnhub_client.company_news(ticker, _from=start_date, to=end_date)
     return news
