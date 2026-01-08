e29b07968ca3358b1117a7cc340cea351cd2d389
iangill1 <i.gill3@universityofgalway.ie>
2026-01-08 21:29:06 +0000

    restructure

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from data_retrieval import price_retrieval, news_retrieval
-
+from naive_model import naive_model_forecast
 def main():
-    company_data = price_retrieval("aapl", "2020-01-01", "2023-01-01", "1d")
-    company_news = news_retrieval("ttwo", "2025-01-01", "2026-01-01")
 
-    print(company_data.tail())
-    print(company_news)
+    # call price retrieval function to retrieve data
+    price_data = price_retrieval("ttwo", "2025-01-01", "2026-01-01", "1d")
+    news_data = news_retrieval("ttwo", "2025-01-01", "2026-01-01")
+
+    rmse, mae = naive_model_forecast(price_data)
+    print("rmse: ", rmse)
+    print("mae: ", mae)
 
 if __name__ == "__main__":
     main()
--- naive_model.py
+++ naive_model.py
@@ -1,32 +1,35 @@
 import pandas as pd
 import numpy as np
-import yfinance as yf
-from sklearn import metrics
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
+from data_retrieval import price_retrieval
 
-#download historical stock data for a company using yfinance api
-data = yf.download("ttwo", interval="60m", start=("2025-11-05"), auto_adjust=False)
+def naive_model_forecast(price_data):
+    #download historical stock data for a company using yfinance api
+    data = price_data
 
-print(data.tail())
+    if "Close" not in data.columns:
+        raise ValueError("Data does not contain 'Close' column")
 
-#shift the close column down by 1 so that each days forecast is yesterdays value
-data['Naive Forecast'] = data['Close'].shift(1)
-#this means first value will be NaN, so drop it
-data = data.dropna()
+    #create copy of data frame to avoid modifying original data
+    data = data.copy()
+    #shift the close column down by 1 so that each days forecast is yesterdays value
+    data['Naive Forecast'] = data['Close'].shift(1)
+    #this means first value will be NaN, so drop it
+    data = data.dropna()
 
-#calculate rmse and mae
-rmse = np.sqrt(mean_squared_error(data['Close'], data['Naive Forecast']))
-mae = mean_absolute_error(data['Close'], data['Naive Forecast'])
+    #calculate rmse and mae
+    rmse = np.sqrt(mean_squared_error(data["Close"], data["Naive Forecast"]))
+    mae = mean_absolute_error(data["Close"], data["Naive Forecast"])
 
-#print results
-print("rmse: ", rmse)
-print("mae: ", mae)
+    return rmse, mae
 
+"""
 #plot results
-plt.figure(figsize=(10, 5))
-plt.plot(data['Close'], label='Actual')
-plt.plot(data['Naive Forecast'], label='Naive Forecast', linestyle='--')
-plt.legend()
-plt.title('Naive Model Forecast vs Actual')
-plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.plot(data['Close'], label='Actual')
+    plt.plot(data['Naive Forecast'], label='Naive Forecast', linestyle='--')
+    plt.legend()
+    plt.title('Naive Model Forecast vs Actual')
+    plt.show()
+"""
