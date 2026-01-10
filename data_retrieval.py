d672228513a870fcba2f3fe1d9d1287cf426eaf6
iangill1 <i.gill3@universityofgalway.ie>
2026-01-10 18:04:11 +0000

    added another function to get news from a 2nd api

--- data_retrieval.py
+++ data_retrieval.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import yfinance as yf
 import finnhub
 import os
+import requests
 
 #load my env file
 load_dotenv()
@@ -12,10 +13,31 @@ def price_retrieval(ticker, start_date, end_date, interval):
     return data
 
 #retrieve news articles from finnhub api
-def news_retrieval(ticker, start_date, end_date):
+def finnhub_news_retrieval(ticker, start_date, end_date):
     #get my api key from env file
     finnhub_key = os.getenv("FINNHUB_API_KEY")
     #create finnhub client
     finnhub_client = finnhub.Client(api_key=finnhub_key)
     news = finnhub_client.company_news(ticker, _from=start_date, to=end_date)
     return news
+
+def alpha_vantage_news_retrieval(ticker, start_date, end_date):
+    #get api key from env file
+    alpha_vantage_key = os.getenv("ALPHA_VANTAGE_API_KEY")
+    #make request to alpha vantage news sentiment endpoint
+    url = 'https://www.alphavantage.co/query'
+    #set parameters for request
+    params = {
+        "function": "NEWS_SENTIMENT",
+        "tickers": ticker,
+        "time_from": start_date,
+        "time_to": end_date,
+        "limit": "1000",
+        "apikey": alpha_vantage_key
+
+    }
+    #make get request
+    response = requests.get(url, params)
+    #parse json response
+    data = response.json()
+    return data
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from data_retrieval import price_retrieval, news_retrieval
+from data_retrieval import price_retrieval, finnhub_news_retrieval, alpha_vantage_news_retrieval
 from naive_model import naive_model_forecast
 def main():
 
     # call price retrieval function to retrieve data
     price_data = price_retrieval("ttwo", "2025-01-01", "2026-01-01", "1d")
-    news_data = news_retrieval("ttwo", "2025-01-01", "2026-01-01")
+    news_data = finnhub_news_retrieval("ttwo", "2025-01-01", "2026-01-01")
 
-    rmse, mae = naive_model_forecast(price_data)
-    print("rmse: ", rmse)
-    print("mae: ", mae)
+    alpha_vantage_news = alpha_vantage_news_retrieval("TTWO", "20250101T0000", "20260101T0000")
+    print(alpha_vantage_news)
+    #rmse, mae = naive_model_forecast(price_data)
+    #print("rmse: ", rmse)
+    #print("mae: ", mae)
 
 if __name__ == "__main__":
     main()
