386247c0335e237e5786fdd35e2d38689abbc856
iangill1 <i.gill3@universityofgalway.ie>
2026-01-19 20:13:12 +0000

    process price retrieval to only include business days

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from dotenv import load_dotenv
 import yfinance as yf
 import finnhub
@@ -7,10 +8,18 @@ import requests
 #load my env file
 load_dotenv()
 
+
 #retrieve historical stock price data from yfinance api
 def price_retrieval(ticker, start_date, end_date, interval):
     data = yf.download(ticker, start=start_date, end=end_date, interval=interval, auto_adjust=False)
+    #convert to datetime index
+    data.index = pd.to_datetime(data.index)
+    #set frequency to business day
+    data = data.asfreq("B")
+    #forward fill missing data (holidays, etc)
+    data = data.ffill()
     return data
+
 
 #retrieve news articles from finnhub api
 def finnhub_news_retrieval(ticker, start_date, end_date):
@@ -21,6 +30,8 @@ def finnhub_news_retrieval(ticker, start_date, end_date):
     news = finnhub_client.company_news(ticker, _from=start_date, to=end_date)
     return news
 
+
+#retrieve news articles from alpha vantage api
 def alpha_vantage_news_retrieval(ticker, start_date, end_date):
     #get api key from env file
     alpha_vantage_key = os.getenv("ALPHA_VANTAGE_API_KEY")
