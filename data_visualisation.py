0a7a0a28deef55b048ed8b7fdbb83d175711f884
iangill1 <i.gill3@universityofgalway.ie>
2026-01-30 17:03:28 +0000

    made the title dynamic

--- data_visualisation.py
+++ data_visualisation.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 
@@ -9,14 +10,22 @@ def plot_stock_price(price_data):
     #error handling
     if data.empty:
         raise ValueError("Price data is empty")
+
+    #get ticker symbol from dataframe and pass to graph title
+    if isinstance(data.columns, pd.MultiIndex):
+        ticker = data.columns.get_level_values(1)[0]
+        data.columns = data.columns.get_level_values(0)
+    else:
+        ticker = "Stock"
+
     if "Close" not in data.columns:
         raise ValueError("Data does not contain 'Close' column")
 
     #plot closing price over time
     plt.figure(figsize=(12, 6))
-    plt.plot(data.index, data['Close'])
-    plt.title('S&P 500 Closing Price Over Time')
-    plt.xlabel('Date')
-    plt.ylabel('Closing Price ($)')
+    plt.plot(data.index, data["Close"])
+    plt.title(f"{ticker} Price Over Time")
+    plt.xlabel("Date")
+    plt.ylabel("Closing Price ($)")
     plt.grid(True)
     plt.show()
