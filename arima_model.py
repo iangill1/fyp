e69201838fdf03faf300d436797ee9919a0810ab
iangill1 <i.gill3@universityofgalway.ie>
2026-02-02 19:50:05 +0000

    added functions to determine d value

--- arima_model.py
+++ arima_model.py
@@ -4,28 +4,45 @@ from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import adfuller
 import matplotlib.pyplot as plt
+from pmdarima.arima.utils import ndiffs
 
 
 def arima_model_forecast(price_data):
     #create copy of data frame to avoid modifying original data
     data = price_data.copy()
+    data1 = data[["Close"]].copy()
+    data2 = data1.describe()
+    print("Describe", data2)
 
     if "Close" not in data.columns:
         raise ValueError("Data does not contain 'Close' column")
 
     #fit ARIMA model
-    model = ARIMA(data['Close'], order=(6, 1, 3))
+    model = ARIMA(data["Close"], order=(6, 1, 3))
     arima_prediction = model.fit()
     print(arima_prediction.summary())
     #get predictions
-    rmse = np.sqrt(mean_squared_error(data['Close'], arima_prediction.fittedvalues))
-    mae = mean_absolute_error(data['Close'], arima_prediction.fittedvalues)
+    rmse = np.sqrt(mean_squared_error(data["Close"], arima_prediction.fittedvalues))
+    mae = mean_absolute_error(data["Close"], arima_prediction.fittedvalues)
 
     return rmse, mae
 
 
-def plot_data_acf(price_data):
-    plot_acf(price_data['Close'].diff().dropna())
+def plot_autocorrelation(price_data):
+    plot_acf(price_data["Close"].diff().dropna())
     plt.show()
-    plot_pacf(price_data['Close'])
-    plt.show()
+    #plot_pacf(price_data['Close'])
+    #plt.show()
+
+
+def adf_test(price_data):
+    df = price_data.copy()
+    close_price = df["Close"]
+    result = adfuller(close_price.diff().dropna())
+    print(f"ADF Statistic: {result[0]}")
+    print(f"p-value: {result[1]}")
+
+
+def determine_differencing(price_data):
+    close_price = price_data["Close"].copy()
+    ndiffs(close_price, test="adf")
