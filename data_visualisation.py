4aada6ddac0fd2ddf0f3efcefe52d17f77b0839a
iangill1 <i.gill3@universityofgalway.ie>
2026-02-03 17:36:46 +0000

    added function to plot trading volume of stock

--- data_visualisation.py
+++ data_visualisation.py
@@ -21,11 +21,41 @@ def plot_stock_price(price_data):
     if "Close" not in data.columns:
         raise ValueError("Data does not contain 'Close' column")
 
+    close_price = data["Close"]
+
     #plot closing price over time
     plt.figure(figsize=(12, 6))
-    plt.plot(data.index, data["Close"])
+    plt.plot(data.index, close_price.diff().diff())
     plt.title(f"{ticker} Price Over Time")
     plt.xlabel("Date")
     plt.ylabel("Closing Price ($)")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_trading_volume(price_data):
+    # make copy of data
+    data = price_data.copy()
+
+    # error handling
+    if data.empty:
+        raise ValueError("Price data is empty")
+
+    # get ticker symbol from dataframe and pass to graph title
+    if isinstance(data.columns, pd.MultiIndex):
+        ticker = data.columns.get_level_values(1)[0]
+        data.columns = data.columns.get_level_values(0)
+    else:
+        ticker = "Stock"
+
+    if "Volume" not in data.columns:
+        raise ValueError("Data does not contain 'Volume' column")
+
+    # plot closing price over time
+    plt.figure(figsize=(12, 6))
+    plt.plot(data.index, data["Volume"])
+    plt.title(f"{ticker} Price Over Time")
+    plt.xlabel("Date")
+    plt.ylabel("Volume ($)")
     plt.grid(True)
     plt.show()
