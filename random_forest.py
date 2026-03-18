3d36227b2fbaa5ed48c3d2c97822638cac1bafeb
iangill1 <i.gill3@universityofgalway.ie>
2026-03-18 19:47:54 +0000

    implemented random forest model with sentiment function

--- random_forest.py
+++ random_forest.py
@@ -59,3 +59,91 @@ def random_forest_default_forecast(price_data):
     plot_actual_vs_predicted(data, pd.DataFrame({"Predicted": predictions}, index=test_close.index))
 
     return rmse, mae, r2
+
+
+def random_forest_sentiment_forecast(price_data, news_sentiment):
+    data = price_data.copy()
+    news_data = news_sentiment.copy()
+
+    if "Close" not in data.columns:
+        raise ValueError("Data does not contain 'Close' column")
+
+    close_price = data["Close"]
+    if isinstance(close_price, pd.DataFrame):
+        close_price = close_price.iloc[:, 0]
+
+    close_price = close_price.astype(float)
+
+    # aggregate sentiment scores by date
+    news_data["datetime"] = pd.to_datetime(news_data["datetime"])
+    news_data = news_data.set_index("datetime")
+    news_data.index = news_data.index.normalize()
+
+    average_sentiment = news_data["sentiment_score"].groupby(news_data.index).mean()
+    price_dates = pd.to_datetime(price_data.index).normalize()
+
+    # define dependent features
+    exog_features = ["Open", "High", "Low", "Volume"]
+    available_features = [f for f in exog_features if f in data.columns]
+
+    if not available_features:
+        raise ValueError("Data does not contain any dependent features (Open, High, Low, Volume)")
+
+    exog_data = data[available_features].astype(float)
+
+    # split data into train and test sets (80/20 split)
+    train_size = int(len(close_price) * 0.8)
+
+    train_close = list(close_price.iloc[:train_size])
+    test_close = close_price.iloc[train_size:]
+
+    train_exog = exog_data.iloc[:train_size]
+    test_exog = exog_data.iloc[train_size:]
+
+    predictions = []
+
+    for t in range(len(test_close)):
+        # dependent for training must match length of train_close
+        current_train_exog = exog_data.iloc[:train_size + t]
+        current_train_close = close_price.iloc[:train_size + t]
+
+        model = RandomForestRegressor(random_state=42).fit(current_train_exog, current_train_close)
+
+        # forecast one step ahead using next row of exog features
+        next_exog = test_exog.iloc[[t]]
+        yhat = model.predict(next_exog)[0]
+
+        current_date = price_dates[train_size + t]
+
+        if current_date in average_sentiment.index:
+            sentiment_score = average_sentiment[current_date]
+            sentiment_adjustment = sentiment_score * yhat * 0.01
+            prediction = yhat + sentiment_adjustment
+        else:
+            prediction = yhat
+
+        predictions.append(prediction)
+        actual_value = test_close.iloc[t]
+        train_close.append(actual_value)
+
+    rmse = np.sqrt(mean_squared_error(test_close, predictions))
+    mae = mean_absolute_error(test_close, predictions)
+    r2 = r2_score(test_close, predictions)
+
+    plot_actual_vs_predicted(data, pd.DataFrame({"Predicted": predictions}, index=test_close.index))
+
+    return rmse, mae, r2
+
+    """
+    #predictions = []
+    model = RandomForestRegressor(random_state=42).fit(train_exog, train_close)
+    predictions = model.predict(test_exog)
+    rmse = np.sqrt(mean_squared_error(test_close, predictions))
+    mae = mean_absolute_error(test_close, predictions)
+    r2 = r2_score(test_close, predictions)
+
+    plot_actual_vs_predicted(data, pd.DataFrame({"Predicted": predictions}, index=test_close.index))
+
+    return rmse, mae, r2
+    """
+
